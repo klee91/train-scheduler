4fa69e7125729299b5faa3c0c408abfa584c90e2
Kevin Lee <lee.kev1991@gmail.com>
2017-03-15 15:16:01 -0400

    cleaned up unecessary lingering comments and code

--- assets/javascript/trainapp.js
+++ assets/javascript/trainapp.js
@@ -8,20 +8,23 @@ var config = {
   storageBucket: "train-scheduler-53c53.appspot.com",
   messagingSenderId: "705870662535"
 };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
+//Train Add Button function
 $("#addButton").on("click", function(event) {
 	event.preventDefault();
 
+	//train input declarations
 	var trainName = $('#addTrainName').val().trim();
 	var trainDest = $('#addDestination').val().trim();
 	var trainFirst = $('#addFirstTrain').val().trim();
 	var trainFirstUnix = parseInt(moment($('#addFirstTrain').val().trim(),"HH:mm").format('X'));
 	var trainFreq = $('#addFrequency').val().trim();
-	console.log(trainFirstUnix)
 
+	//Train object with inputs
 	var newTrain = {
 		name: trainName,
 		dest: trainDest,
@@ -30,30 +33,28 @@ $("#addButton").on("click", function(event) {
 		freq: trainFreq
 	};
 
-	console.log(newTrain.name);
-	console.log(newTrain.dest);
-	console.log(newTrain.first);
-	console.log(newTrain.firstunix);
-	console.log(newTrain.freq);
-
+	//Train object push to database
 	database.ref().push(newTrain);
 
+	//clear input fields upon submission
 	$("#addTrainName").val(" ");
   	$("#addDestination").val(" ");
   	$("#addFirstTrain").val(" ");
   	$("#addFrequency").val(" ");
 
   	return false;
-})
+});
 
-
-database.ref().on("child_added" , function(childSnapshot, prevChildKey) {
+//Function when data is pushed to database
+database.ref().on("child_added" , function(childSnapshot) {
+	//Shorthand convenience
 	var snap = childSnapshot.val();
 
-	var trainName = snap.name;//Bay Express
-	var trainDest = snap.dest;//NYC
-	var trainFirst = snap.first;//12:00
-	var trainFirstUnix = parseInt(snap.firstunix);//unix #
+
+	var trainName = snap.name;
+	var trainDest = snap.dest;
+	var trainFirst = snap.first;
+	var trainFirstUnix = parseInt(snap.firstunix);
 	var trainFreq = snap.freq;
 	
 	//first train conversion to total minutes
@@ -64,8 +65,7 @@ database.ref().on("child_added" , function(childSnapshot, prevChildKey) {
 	//conversion to unix
 	var currentTimeUnix = parseInt(moment().format('X'));
 	currentTime = unixConv(currentTimeUnix);
-	console.log(currentTime)
-	console.log(currentTimeUnix)
+	
 //----------------------------------------------------------------
 //In minutes, total of first train time & current time from 00:00
 	var trainFirstTotal = ((trainFirstHours * 60) + trainFirstMins);
@@ -82,33 +82,24 @@ database.ref().on("child_added" , function(childSnapshot, prevChildKey) {
 	//get difference 
 	var diff = currentTimeTotal - trainFirstTotal // mins
 	var diffUnix = (currentTimeUnix - trainFirstUnix); // unix seconds
-	var diffTime = moment.unix(diff).format('HH:mm'); // HH:mm
-	console.log(diff)
 	
 	// The nth train that has already arrived
-	var x = Math.floor(diff / trainFreq);//10th
-	var y = (trainFreq * x) //mins of latest train from first train
+	var x = Math.floor(diff / trainFreq);
+	var y = (trainFreq * x) 
 
 	//next train in mins from first train
 	var z = (trainFreq * (x + 1))
-	console.log(z)
 //----------------------------------------------------------------
-//	
 
 	//next train arrival in seconds from first train departure
 	var nextTrainUnix = ((z * 60) + trainFirstUnix);
-	console.log(nextTrainUnix)
 	
-	console.log(currentTimeUnix);
-	//
 	var yy = (nextTrainUnix - currentTimeUnix)
 	var trainArrival = unixConv(nextTrainUnix);
 
-	//number of minutes until next train arrival
-	
+	//mins until next train arrival from current time, rounded up
 	var minsAway = Math.ceil((nextTrainUnix - currentTimeUnix) / 60);
 	
-	// var minsAway = (diff % trainFreq);
   // Add train's data into the table
   $(".train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDest + "</td><td>" +
   trainFreq + "</td><td>" + trainArrival + "</td><td>" + minsAway + "</td><tr>");
@@ -118,7 +109,6 @@ database.ref().on("child_added" , function(childSnapshot, prevChildKey) {
 		var x = moment.unix(x).format("HH:mm");
 		return x;
 	}
-
 
 });
 
